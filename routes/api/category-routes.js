c8c57a4e10b48763d9935da75b8a5c87486e0efa
Anna <acdodd17@gmail.com>
2022-01-15 08:22:49 -0500

    tag-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -85,7 +85,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'No category fournd with this id!'});
+      res.status(404).json({ message: 'No category found with this id!'});
       return;
     }
     res.json(dbCategoryData);
